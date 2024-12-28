bd5293606719663d06693d3aff7595f6582704ed
Henry Odunsi <henryodunsi05@gmail.com>
2024-12-28 12:38:03 +0100

    feat: push

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 AOS.init();
+
+
+const compliment = () => {
+
+    setTimeout(function thanks() {
+        let popup = document.querySelector('.popup');
+        popup.classList.add('pop')
+        popup.classList.remove('popup')
+    }, 2000);
+}
