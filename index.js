61d31efd958ddeb8d34b20bfb5cfa999884c18cf
Henry Odunsi <henryodunsi05@gmail.com>
2024-12-30 13:54:02 +0100

    timeout

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ AOS.init();
 
 
 const compliment = () => {
-
     setTimeout(function thanks() {
         let popup = document.querySelector('.popup');
         popup.classList.add('pop')
         popup.classList.remove('popup')
+        setTimeout(() => {
+            popup.classList.remove('pop')
+            popup.classList.add('popup')
+        }, 2000);
     }, 2000);
+
 }
+
 
 
 
