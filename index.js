2922c7a9c24e5709146822329deacca68f1cdb79
Henry Odunsi <henryodunsi05@gmail.com>
2025-01-05 17:55:40 +0100

    format

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 AOS.init();
 
-
 const compliment = () => {
-    setTimeout(function thanks() {
-        let popup = document.querySelector('.popup');
-        popup.classList.add('pop')
-        popup.classList.remove('popup')
-        setTimeout(() => {
-            popup.classList.remove('pop')
-            popup.classList.add('popup')
-        }, 6000);
-    }, 2000);
+  setTimeout(function thanks() {
+    let popup = document.querySelector(".popup");
+    popup.classList.add("pop");
+    popup.classList.remove("popup");
+    setTimeout(() => {
+      popup.classList.remove("pop");
+      popup.classList.add("popup");
+    }, 6000);
+  }, 2000);
+};
 
-}
- 
-let footer = document.querySelector('.year');
+let footer = document.querySelector(".year");
 const date = new Date().getFullYear();
 footer.innerText = date;
-
-
